21f9f03aac32bf86b7ce35c6ddebd081ddaad882
mariiavitoriaribeiro <mariaviviviravento1509@gmail.com>
2023-05-09 21:32:54 -0300

    atualizacao excluir e alterar

--- resources/views/gerenciadorFuncionario.blade.php
+++ resources/views/gerenciadorFuncionario.blade.php
@@ -1,9 +1,7 @@
 @extends('padrao')
-
 @section('content')
 
 <div class="container mt-5">
-
 <form method="get" action="{{route('gerenciar-funcionario')}}">
   <div class="mb-3 row">
     <label for="inputPesquisar" class="col-sm-2 col-form-label">Pesquisar:</label>
@@ -12,62 +10,53 @@
     </div>
     <div class="col-sm-2"> <button type="submit" class="btn btn-outline-primary">Pesquisar</button> </div>
   </div>
-
 </form>
 
 <table class="table table-dark table-hover">
   <thead>
     <tr>
-      <td>Codígo</td>
-      <td>Nome</td>
-      <td>Senha</td>
-      <td>Email</td>
-      <td>Whatsapp</td>
+      <th scope="col">Código</th>
+      <th scope="col">Nome</th>
+      <th scope="col">E-mail</th>
+      <th scope="col">Alterar</th>
+      <th scope="col">Excluir</th>
     </tr>
   </thead>
   <tbody>
-    @if(empty($_GET['nomefun']))
-    @else
     @foreach($dadosfuncionario as $dadosfuncionarios)
-    
     <tr>
       <th scope="row">{{$dadosfuncionarios->id}}</th>
       <td>{{$dadosfuncionarios->nomefun}}</td>
       <td>{{$dadosfuncionarios->emailfun}}</td>
-      <td><a href="{{route('mostrar-funcionario',$dadosfuncionarios->id)}}">Alterar</a></td>
       <td>
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-Excluir
-</button>
-      </td>
+        
+      <a href="{{route('mostrar-funcionario',$dadosfuncionarios->id)}}">Alterar</a>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalAlterarFun-{{$dadosfuncionarios->id}}">
+          Alterar
+        </button>
+
+@include('modal.funcionarioAlterar')
+
+    
+    </td>
+      <td>
+
+      <!-- Button trigger modal -->
+        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalDeleteFun-{{$dadosfuncionarios->id}}">
+          Excluir
+        </button>
+
+@include('modal.funcionarioDeletar')
+
+
+
+      </td>  
+
     </tr>
-
-
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Cuidado!</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Deseja mesmo excluir?
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-        <form method="post" action="{{route('apagar-funcionario',$dadosfuncionarios->id)}}">
-        @method('delete')
-        @csrf 
-        <button type="submit" class=" btn btn-danger">Excluir</button>
-      </form>
-      </div>
-    </div>
-  </div>
-</div>
-
    @endforeach
   </tbody>
 </table>
+
 </div>
-@endif
 @endsection
---
+++ resources/views/modal/funcionarioAlterar.Blade.php
@@ -0,0 +1,50 @@
+<!-- Modal -->
+<div class="modal fade" id="modalAlterarFun-{{$dadosfuncionarios->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5 text-dark" id="exampleModalLabel">Alterar Funcionário</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body text-dark">
+      <form method = "post" action="{{route('cadastro-funcionario')}}">
+    @csrf
+            <div class="mb-3 form-check">
+                <label for="nomeInput" class="form-label">Nome:</label>
+                <input type="text" name="nomefun" value="{{$dadosfuncionarios->nomefun}}" class="form-control" id="nomeInput" >
+            </div>
+
+            <div class="mb-3 form-check">
+                <label for="emailInput" class="form-label">Email:</label>
+                <input type="email" name="emailfun" class="form-control" id="emailInput">
+            </div>
+
+            <div class="mb-3 form-check">
+                <label for="whatsappInput" class="form-label">Whatsapp:</label>
+                <input type="text" name="whatsappfun"class="form-control" id="whatsappInput" >
+            </div>
+            
+            <div class="mb-3 form-check">
+                <label for="cpfInput" class="form-label">CPF:</label>
+                <input type="text" name="cpffun" class="form-control" id="cpfInput">
+            </div>
+
+            <div class="mb-3 form-check">
+                <label for="senhaInput" class="form-label">Senha:</label>
+                <input type="password" name="senhafun"class="form-control" id="senhaInput">
+            </div>
+
+           
+            <button type="submit" class="btn btn-primary">Salvar</button>
+        </form>
+
+      
+    
+    
+    </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+      </div>
+    </div>
+  </div>
+</div>
